4f7a2962d0cc9f85ab5c1a2f1f92922ade95c385
htactive-thuonght <thuonght@htactive.com>
2019-06-18 15:30:39 +0700

    practice

---
+++ promise.js
@@ -0,0 +1,88 @@
+let aPromise = new Promise((resolve, reject )=>{
+    // resolve("Thành công");
+   
+  
+        // reject("lỗi rồi em")
+        resolve("thành công rồi")
+
+});
+ 
+aPromise.then((msg)=>console.log('Đã thực thi ' +msg),
+(errMsg)=>console.log(errMsg + ' '));
+
+
+
+
+let add = (a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if (typeof a !="number"||typeof b != 'number') {
+                reject(new Error("Tham số phải kiểu number"))
+            }
+            resolve(a+b);
+        }, 1000);
+    })
+}
+add("1",4)
+.then((result)=> console.log("kết quả là: " +result),(error)=>console.log(error + '' ))
+
+
+let multiply =(a,b)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if (typeof a != 'number'||typeof b != 'number' ) {
+                reject(new Error("Tham số phải kiểu number nhé")
+                )
+            }
+            resolve(a*b)
+        }, 1000);
+    })
+}
+let divide =(a,b)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if (typeof a != 'number'||typeof b != 'number' ) {
+                reject(new Error("Tham số phải kiểu number nhé")
+                )
+            }
+            if (b===0) {
+                reject(new Error('Chia cho 0 nè'))
+            }
+            resolve(a/b)
+        }, 1000);
+    })
+}
+
+// divide(1,4)
+// .then((result)=> console.log("kết quả là: " +result),(error)=>console.log(error + '' )
+
+// )
+
+// multiply(1,4)
+// .then((result)=> console.log("kết quả là: " +result),(error)=>console.log(error + '' )
+
+// )
+// add("1",4)
+// .then((result)=> console.log("kết quả là: " +result),(error)=>console.log(error + '' )
+
+// )
+//sử dụng proise liên tiếp
+    let dientich = (a,b,h)=>{
+        add(a,b)
+        .then(result=>multiply(result,h))
+        .then(res=> divide(res,2))
+        .then(square=>console.log("Diện tích: " +square ))
+        .catch(e=>console.log(e+''))
+    }
+    dientich(3,4,5)
+
+// tái sử dụng function
+
+let dientich1 = (a,b,h)=>{
+    return  add(a,b)
+            .then(result=>multiply(result,h))
+            .then(res=> divide(res,2))
+}
+dientich1(6,4,5)
+    .then(square=>console.log("Diện tích: " +square ))
+    .catch(e=>console.log(e+''))
